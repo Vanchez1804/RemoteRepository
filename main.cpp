65fe4b839c6613563fb3564574c5c5c2f8eb7d31
Vanchez1804 <126703602+Vanchez1804@users.noreply.github.com>
2024-12-18 16:26:57 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+// Простая функция сложения
+int Add(int a, int b) {
+    return a + b;
+}
+
+// Простая функция вычитания
+int Subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
-    std::cout << "Hello, GitHub Actions!" << std::endl;
+    std::cout << "Hello, CI Testing!" << std::endl;
     return 0;
 }
+
  
