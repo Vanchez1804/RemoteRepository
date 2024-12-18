b3bd1a7f9628d42d6fca45c79a5617e88118690e
Vanchez1804 <126703602+Vanchez1804@users.noreply.github.com>
2024-12-18 19:00:21 +0300

    test.cpp

--- test.cpp
+++ test.cpp
@@ -12,18 +12,9 @@ void TestSubtract() {
     std::cout << "TestSubtract passed!" << std::endl;
 }
 
-// Новый тест для функции Square
-void TestSquare() {
-    assert(Square(2) == 4);  // 2 * 2 должно быть равно 4
-    assert(Square(-3) == 9); // -3 * -3 должно быть равно 9
-    assert(Square(0) == 0);  // 0 * 0 должно быть равно 0
-    std::cout << "TestSquare passed!" << std::endl;
-}
-
 int main() {
     TestAdd();
     TestSubtract();
-    TestSquare(); // Запуск нового теста
     std::cout << "All tests passed!" << std::endl;
     return 0;
 }
